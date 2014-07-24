bd1e1206f2896bf6bb3dbb58e74f7164d5e8a1d1
Mukesh <mukesh@nfnlabs.in>
2014-07-24 18:24:56 +0530

    scenarios in

--- scripts/controllers/scenarios.js
+++ scripts/controllers/scenarios.js
@@ -1,7 +1,12 @@
 'use strict';
 
 angular.module('stackApp')
-  .controller('ScenariosCtrl', function ($scope, Authentication, $location) {
+  .controller('ScenariosCtrl', function ($scope, Authentication, $location, scenarios) {
+        scenarios.loadScenario().then(function() {
+          $scope.loaded = true;
+          $scope.load = "Load";
+          $scope.scenarios = scenarios.getScenario();
+        });
         $scope.stack = Authentication.getUser();
         $scope.logout = function () {
           Authentication.logout();
--- scripts/services/scenarios.js
+++ scripts/services/scenarios.js
@@ -8,16 +8,37 @@
  * Factory in the stackApp.
  */
 angular.module('stackApp')
-  .factory('scenarios', function () {
+  .factory('scenarios', function ($q, $http, $cookieStore, $cookies) {
     // Service logic
     // ...
-
-    var meaningOfLife = 42;
-
-    // Public API here
+    var scenario = null;
     return {
-      someMethod: function () {
-        return meaningOfLife;
+      loadScenario : function()
+      {
+          var deferred = $q.defer();
+          var sessionCookie = $cookies.sessionKey;
+          var apikey = '&apikey=Q6yO1lGelAEaP9cT/M3mbQ==';
+          var projectID = '&ProjectID=590';
+          console.log(sessionCookie);
+          var sessionKey = '&sessionKey='+sessionCookie;
+          $http({
+            method  : 'POST',
+            url     : 'http://apps.impetusanalytics.com/mmmstackweb/Service1.asmx/scenarios',
+            data    : apikey+sessionKey+projectID,
+            headers : { 'Content-Type': 'application/x-www-form-urlencoded' }
+            })
+            .success(function(scenarioData) {
+              console.log(scenarioData);
+              scenario = scenarioData;
+              deferred.resolve(scenario);
+          }).error(function(error)
+            {
+                deferred.reject(error);
+          });
+          return deferred.promise;
+      },
+      getScenario : function () {
+        return scenario;
       }
-    };
+    }
   });
